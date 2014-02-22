54529e1e993ca0c204e72a2ab05f12de9192a029
fosterdill <fosterdill@gmail.com>
2014-02-21 23:22:08 -0800

    add auth methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,45 @@
 class User < ActiveRecord::Base
+  validates :email, :presence => true
+  validates :age, :numericality => true
+  validates
+    :password,
+    :presence => true,
+    :length => { :minimum => 6 },
+    :on => :create
+
+    before_validation :require_session_token
+
+  attr_reader :password
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by_email(email)
+
+    if user && user.is_password?(password)
+      return user
+    end
+
+    nil
+  end
+
+  def require_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+  end
+
+  def password=(pt_password)
+    @password = pt_password
+    self.password_digest = BCrypt::Password.create(pt_password)
+  end
+
+  def is_password?(pt_password)
+    BCrypt::Password.new(self.password_digest).is_password?(pt_password)
+  end
 end
