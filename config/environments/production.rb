aef5e3c831fc2c285a355b6be2545750b023e46b
jacknoble <jack.noble@gmail.com>
2014-03-06 17:29:05 -0800

    add image path helper for rails 4 deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,10 +78,6 @@ Coffeend::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   config.assets.initialize_on_precompile = false
-  config.cache_classes = true
-  config.serve_static_assets = true
-  config.assets.compile = true
-  config.assets.digest = true
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
