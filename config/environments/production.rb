d8d6bc336375a3dea7380631fad199c118f420bb
jacknoble <jack.noble@gmail.com>
2014-03-06 17:16:04 -0800

    heroku tweak

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Coffeend::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
+  config.assets.initialize_on_precompile = false
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
