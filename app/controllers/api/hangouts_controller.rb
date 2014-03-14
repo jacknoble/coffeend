c97deba945ea2fcad21b09f16f793b1b65e0a676
jacknoble <jack.noble@gmail.com>
2014-03-13 20:50:15 -0700

    fix datetime bug that was reversing day and month, hangouts congtroller now renders jbuilder template

--- app/controllers/api/hangouts_controller.rb
+++ app/controllers/api/hangouts_controller.rb
@@ -4,9 +4,11 @@ class Api::HangoutsController < ApplicationController
 
   def create
     params[:hangout][:user_id] = current_user.id
+    date = params[:hangout][:start]
+    params[:hangout][:start] = DateTime.strptime(date, '%m/%d/%Y %H:%M %p')
     @hangout = Hangout.new(params[:hangout].permit!)
     if @hangout.save
-      render :json => @hangout
+      render :partial => 'api/hangouts/hangout'
     else
       render :json => @hangout.errors.full_messages,
       :status => :unprocessable_entity
--- app/views/api/hangouts/_hangout.json.jbuilder
+++ app/views/api/hangouts/_hangout.json.jbuilder
@@ -1,3 +1,4 @@
+hangout ||= @hangout
 json.(hangout, :id, :user_id, :start, :end, :location_name, :description, :lat, :lng, :created_at, :updated_at)
 json.attending_users do 
 	json.array! hangout.attending_users do |user|
