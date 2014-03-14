823c846d973bcc909a603e0525c7b00c6e62149f
jacknoble <jack.noble@gmail.com>
2014-03-13 22:16:16 -0700

    user and session view stling

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,5 +1,5 @@
 class Attendance < ActiveRecord::Base
-	validates_uniqueness_of :user_id, :scope => :account_id
+	validates_uniqueness_of :user_id, :scope => :hangout_id
 
 	belongs_to :user
 	belongs_to :hangout
